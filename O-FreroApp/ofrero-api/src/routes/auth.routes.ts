4aa50c234c2a2fc159b0ce45844f630291295a15
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-10-16 10:40:27 +0200

    Adapting new routes to corresponding with the service authentification with prisma

--- O-FreroApp/ofrero-api/src/routes/auth.routes.ts
+++ O-FreroApp/ofrero-api/src/routes/auth.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import { registerUser } from "../services/auth.prisma.service";
-import { loginUser } from "../services/auth.prisma.service";
+import { registerUserPrisma, loginUserPrisma } from "../services/auth.prisma.service";
 
 export const authRouter = Router();
 
 authRouter.post("/register", async (req, res) => {
-  const { status, body } = await registerUser(req.body);
-  res.status(status).json(body);
+  const result = await registerUserPrisma(req.body);
+  res.status(result.status).json(result.body);
 });
 
 authRouter.post("/login", async (req, res) => {
-  const { status, body } = await loginUser(req.body);
-  res.status(status).json(body);
+  const result = await loginUserPrisma(req.body);
+  res.status(result.status).json(result.body);
 });
